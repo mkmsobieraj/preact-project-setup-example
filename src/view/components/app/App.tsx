0865d82f03f6d51444d5c74535fb7f4fdf7c5e7f
Mateusz Sobieraj <core.editor>
2021-12-14 00:04:51 +0100

    #5 [Crate basic layout] - implement task

--- src/view/components/app/App.tsx
+++ src/view/components/app/App.tsx
@@ -2,17 +2,40 @@ import React, { ReactElement } from 'react';
 import styles from '../../../styles.css';
 
 export const App = (): ReactElement => (
-  <div className={styles.page}>
+  <>
     <header className={styles.header}>
       <h1>To Do App</h1>
       <nav className={styles.navBar}></nav>
-      <main>
-        <section className={styles.toDoCart}>
-          <h2 className={styles.toDoCardTitle}></h2>
-          <p className={styles.toDoCardContent}></p>
-        </section>
-      </main>
-      <footer className={styles.footer}></footer>
     </header>
-  </div>
+    <main className={styles.mainContent}>
+      <section className={styles.toDoCart}>
+        <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+        <div className={styles.toDoCardParagraphContainer}>
+          <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+            repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+          </p>
+        </div>
+      </section>
+      <section className={styles.toDoCart}>
+        <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+        <div className={styles.toDoCardParagraphContainer}>
+          <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+            repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+          </p>
+        </div>
+      </section>
+      <section className={styles.toDoCart}>
+        <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+        <div className={styles.toDoCardParagraphContainer}>
+          <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+            fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+            repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+          </p>
+        </div>
+      </section>
+    </main>
+    <footer className={styles.footer}></footer>
+  </>
 );
