842406ba987cc327fdd5a0d171f1a0c853e0aaa4
Mateusz Sobieraj <core.editor>
2021-12-16 00:11:37 +0100

    #18 [Implement paper like view of application] - implement task

--- src/view/components/app/App.tsx
+++ src/view/components/app/App.tsx
@@ -7,35 +7,41 @@ export const App = (): ReactElement => (
       <h1>To Do App</h1>
       <nav className={styles.navBar}></nav>
     </header>
-    <main className={styles.mainContent}>
-      <section className={styles.toDoCart}>
-        <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
-        <div className={styles.toDoCardParagraphContainer}>
-          <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
-            repudiandae pariatur, tenetur esse excepturi illum aspernatur.
-          </p>
-        </div>
-      </section>
-      <section className={styles.toDoCart}>
-        <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
-        <div className={styles.toDoCardParagraphContainer}>
-          <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
-            repudiandae pariatur, tenetur esse excepturi illum aspernatur.
-          </p>
-        </div>
-      </section>
-      <section className={styles.toDoCart}>
-        <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
-        <div className={styles.toDoCardParagraphContainer}>
-          <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-            fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
-            repudiandae pariatur, tenetur esse excepturi illum aspernatur.
-          </p>
-        </div>
-      </section>
-    </main>
+    <div className={styles.mainContentWrapper}>
+      <main className={`${styles.page} ${styles.mainContent}`}>
+        <section className={styles.toDoCart}>
+          <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+          <div className={styles.toDoCardParagraphContainer}>
+            <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+              repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+            </p>
+          </div>
+        </section>
+        <section className={styles.toDoCart}>
+          <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+          <div className={styles.toDoCardParagraphContainer}>
+            <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+              repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+            </p>
+          </div>
+        </section>
+        <section className={styles.toDoCart}>
+          <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+          <div className={styles.toDoCardParagraphContainer}>
+            <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+              repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+            </p>
+          </div>
+        </section>
+      </main>
+      <div className={`${styles.page} ${styles.first}`}></div>
+      <div className={`${styles.page} ${styles.second}`}></div>
+      <div className={`${styles.page} ${styles.third}`}></div>
+      <div className={`${styles.page} ${styles.fourth}`}></div>
+    </div>
     <footer className={styles.footer}></footer>
   </>
 );
