6835b5ee389dcdef0f02cc3f43c0b893202e31d0
Mateusz Sobieraj <core.editor>
2021-12-17 01:45:47 +0100

    Working with styles

--- src/view/components/app/App.tsx
+++ src/view/components/app/App.tsx
@@ -2,13 +2,35 @@ import React, { ReactElement } from 'react';
 import styles from '../../../styles.css';
 
 export const App = (): ReactElement => (
+  
   <>
-    <header className={styles.header}>
-      <h1>To Do App</h1>
-      <nav className={styles.navBar}></nav>
-    </header>
     <div className={styles.mainContentWrapper}>
       <main className={`${styles.page} ${styles.mainContent}`}>
+        <section className={styles.toDoCart}>
+          <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+          <div className={styles.toDoCardParagraphContainer}>
+            <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+              repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+            </p>
+          </div>
+          <p className={styles.tagSection}>
+            <span className={styles.tag}>#TestTag</span>
+            <span className={styles.tag}>#AnotherTag</span>
+            <span className={styles.tag}>#Tag3</span>
+            <span className={styles.tag}>#Tag5</span>
+            <span className={styles.tag}>#Tag6</span>
+          </p>
+        </section>
+        <section className={styles.toDoCart}>
+          <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
+          <div className={styles.toDoCardParagraphContainer}>
+            <p className={styles.toDoCardParagraph}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+              fugiat voluptate labore similique a quos doloribus perspiciatis magnam optio nihil, expedita, rem harum
+              repudiandae pariatur, tenetur esse excepturi illum aspernatur.
+            </p>
+          </div>
+        </section>
         <section className={styles.toDoCart}>
           <div className={styles.toDoCardTitleContainer}><h2 className={styles.toDoCardTitle}>Lorem, ipsum</h2></div>
           <div className={styles.toDoCardParagraphContainer}>
@@ -42,6 +64,5 @@ export const App = (): ReactElement => (
       <div className={`${styles.page} ${styles.third}`}></div>
       <div className={`${styles.page} ${styles.fourth}`}></div>
     </div>
-    <footer className={styles.footer}></footer>
   </>
 );
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,22 +3,18 @@ import { Configuration } from 'webpack';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const distDir = path.resolve(__dirname, 'dist');
-const srcDir = path.resolve(__dirname, 'src');
+const distDir = path.join(__dirname, 'dist');
+const srcDir = path.join(__dirname, 'src');
 
 const config: Configuration = {
   entry: './src/index.tsx',
   output: {
-    path: distDir,
-    filename: '[name].js',
-    assetModuleFilename: '[path][name][ext]',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'http://localhost:8080/',
   },
   module: {
     rules: [
-      {
-        test: /\.(svg|woff)$/,
-        type: 'asset/resource',
-      },
       {
         test: /\.(ts|tsx)$/,
         use: {
@@ -33,9 +29,17 @@ const config: Configuration = {
           'css-modules-typescript-loader',
           {
             loader: 'css-loader',
-            options: { modules: true, import: true, importLoaders: 1 },
+            options: {
+              modules: true,
+              import: true,
+              importLoaders: 1,
+            },
           },
         ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: 'asset',
       },
     ],
   },
@@ -57,7 +61,15 @@ const config: Configuration = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
-    static: './dist',
+    devMiddleware: {
+      index: true,
+      mimeTypes: { phtml: 'text/html' },
+      publicPath: 'http://localhost:8080/',
+      // writeToDisk: true,
+    },
+    static: {
+      directory: distDir,
+    },
     client: {
       overlay: {
         errors: true,
